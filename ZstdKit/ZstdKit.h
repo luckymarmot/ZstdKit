9ee369537f93aa2393922a2f488eb193db233df6
Micha Mazaheri <micha@mazaheri.me>
2018-04-12 22:23:50 +0200

    Initial Commit

---
+++ ZstdKit/ZstdKit.h
@@ -0,0 +1,19 @@
+//
+//  ZstdKit.h
+//  ZstdKit
+//
+//  Created by Micha Mazaheri on 4/12/18.
+//  Copyright © 2018 Paw. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ZstdKit.
+FOUNDATION_EXPORT double ZstdKitVersionNumber;
+
+//! Project version string for ZstdKit.
+FOUNDATION_EXPORT const unsigned char ZstdKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZstdKit/PublicHeader.h>
+
+
